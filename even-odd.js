24a9eeb610d00670face06d6ea5add600e02b22c
Akanksha Choudhary <akanksha.ch29@gmail.com>
2019-02-24 20:51:44 +0530

    even-odds

--- even-odd.js
+++ even-odd.js
@@ -8,5 +8,25 @@ function checkEvenOdd(num) {
     }
 }
 
-const evenOdd=checkEvenOdd(31)
-console.log(evenOdd)
+const evenOdd=checkEvenOdd(31);
+// console.log(evenOdd)
+
+
+// checking the number of even numbers in an array
+
+    const NUMBERS_ARRAY = [3,5,2,6,89,90,4];
+    function numOfEvens(){
+        let count = 0;
+        // traverse the array
+        for(let i = 0; i < NUMBERS_ARRAY.length; i++){
+            if(NUMBERS_ARRAY[i]%2 === 0){
+                 //incrementing a value every time an even number is found
+                count = count + 1;
+            }
+        }
+        return count;
+    }
+     const countOfEvens = numOfEvens();
+     console.log(countOfEvens);
+
+   
