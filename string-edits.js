38d9a78bb08da5875bff801586a0d6165153046a
Akanksha Choudhary <akanksha.ch29@gmail.com>
2019-03-20 23:27:18 +0530

    strings one edit away

--- string-edits.js
+++ string-edits.js
@@ -1,10 +1,34 @@
-// pale -> ple
+// pale -> ple, Given 2 strings, find that they are just one edit away
 
-// check length
-// check for equal characters
-
-function checkEdits(str1, str2) {
-    
+function checkOneEdit(str1, str2) {
+    let arr = [];
+    // if ((str1.length - str2.length) > 1 || (str2.length - str1.length) > 1  ) {
+    //     return false;
+    // }
+    let i = 0; let j = 0; let diff = 0;
+    while(i<str1.length && j<str2.length){
+        if(str1[i] !== str2[j]){
+            if(str1.length > str2.length){
+                arr.push(str1[i])
+                i++;
+                // console.log(i)
+            }
+            if(str1.length < str2.length){
+                arr.push(str1[j])
+                j++;
+                // console.log(j)
+            }
+            if(str1.length === str2.length){
+                arr.push(str1[i])
+                i++; j++;
+                // console.log(i,j)
+            }
+        } else {
+            i++; j++;
+            // console.log('else', i,j)
+        }
+    }
+    return arr;
 }
 
-console.log(checkEdits("pale", "ple"));
+console.log(checkOneEdit("padlse", "ple"));
